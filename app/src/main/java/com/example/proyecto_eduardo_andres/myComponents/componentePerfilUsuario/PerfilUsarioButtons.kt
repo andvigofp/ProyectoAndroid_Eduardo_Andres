c991317fd63ea28d5444c604e4013958bf431d4b
andvigofp <aniamcion96@gmail.com>
2025-10-24 20:42:57 +0200

    Componenete Boton Perfil terminado

---
+++ app/src/main/java/com/example/proyecto_eduardo_andres/myComponents/componentePerfilUsuario/PerfilUsarioButtons.kt
@@ -0,0 +1,71 @@
+package com.example.proyecto_eduardo_andres.myComponents.componentePerfilUsuario
+
+import android.view.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyecto_eduardo_andres.myComponents.componeneteCrearUsuario.CrearUsuarioButton
+import com.example.proyecto_eduardo_andres.myComponents.componeneteCrearUsuario.CrearUsuarioButtonText
+
+@Composable
+fun PerfilUsuarioButtons(
+    perfilUsuarioButtonTextsData: PerfilUsuarioButtonTextsData,
+    onModificarUsuario: () -> Unit,
+) {
+    // Si el data class viene vacío, damos los textos por defecto aquí
+    val modificarUsuarioText =
+        if (perfilUsuarioButtonTextsData.modificar.isNotEmpty()) perfilUsuarioButtonTextsData.modificar else "MODIFICAR USUARIO"
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            onClick = onModificarUsuario,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF4CAF50),
+                contentColor = Color.White
+            ),
+            modifier = Modifier
+                .weight(1f)
+                .height(55.dp)
+        ) {
+            Text(text = modificarUsuarioText, fontSize = 18.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PerfilUsuarioButtonPreview() {
+    MaterialTheme {
+        Surface(color = Color(0xFFF5F5F5)) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                PerfilUsuarioButtons (
+                    perfilUsuarioButtonTextsData = PerfilUsuarioButtonTextsData(),
+                    onModificarUsuario = {},
+                )
+            }
+        }
+    }
+}
