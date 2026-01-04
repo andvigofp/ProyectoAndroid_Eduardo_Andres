8ddee280db2447ee4465a7f57d0b65d8fad5d6bb
quijote2 <perezgarciaeduardo150@gmail.com>
2026-01-04 19:12:23 +0100

    Añadido el viewmodel a la página crear usuario

--- app/src/main/java/com/example/proyecto_eduardo_andres/viewmodel/PerfilUsuarioViewModel.kt
+++ app/src/main/java/com/example/proyecto_eduardo_andres/viewmodel/PerfilUsuarioViewModel.kt
@@ -1,4 +1,36 @@
 package com.example.proyecto_eduardo_andres.viewmodel
 
-class PerfilUsuarioViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.proyecto_eduardo_andres.viewData.perfilUsuarioData.PerfilUsuarioUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class PerfilUsuarioViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(PerfilUsuarioUiState())
+    val uiState: StateFlow<PerfilUsuarioUiState> = _uiState.asStateFlow()
+
+    fun onNombreUsuarioChange(nombre: String) {
+        _uiState.update { it.copy(nombreUsuario = nombre) }
+    }
+
+    fun onEmailChange(email: String) {
+        _uiState.update { it.copy(email = email) }
+    }
+
+    fun onPasswordChange(password: String) {
+        _uiState.update { it.copy(password = password) }
+    }
+
+    fun toggleEditing() {
+        _uiState.update { it.copy(isEditing = !it.isEditing) }
+    }
+
+    fun guardarCambios() {
+        if (_uiState.value.isModificarButtonEnabled) {
+            _uiState.update { it.copy(isEditing = false) }
+        }
+    }
 }
